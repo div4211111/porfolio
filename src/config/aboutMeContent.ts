7d0987f9373750bbcd692a16f18edaada6ba0faf
div4211111 <andrew@Andrews-MacBook-Pro.local>
2023-05-10 21:45:43 +0300

    Change info

--- src/config/aboutMeContent.ts
+++ src/config/aboutMeContent.ts
@@ -1,54 +1,54 @@
 export const aboutMeContent = {
   personalInfo: {
     bio: {
-      myBio: `Hi, I'm Andrew Alexeev! I'm a frontend developer with a real passion for technology. Over the years, I've gained an in-depth understanding of various programming languages, frameworks, and development tools, which has allowed me to deliver high-quality solutions to my clients.
+      myBio: `Hi, I'm Andrew Alexeev! I'm a front-end developer with a real passion for technology. Over the years, I've gained a deep understanding of various programming languages, frameworks, and development tools, which has allowed me to deliver high-quality solutions to my clients.
 
-I've always loved working in the tech industry and feel privileged to have the opportunity to keep up with the latest developments. I'm constantly learning new technologies and applying them to my work to create great user experiences for my clients.
+I've always loved working in the technology industry and feel privileged to have the opportunity to keep up with the latest developments. I'm constantly learning new technologies and applying them to my work to create great user experiences for my clients.
 
-When I'm not working, I have a few hobbies that keep me busy. I'm an avid football fan, and I love watching matches with my friends and family. I also enjoy quizzes and challenging myself on a variety of topics. And when I need to relax, there's nothing better than traveling to new places and experiencing different cultures.
+When I'm not working, I have a few hobbies that keep me busy. I'm an avid soccer fan and love watching games with my friends and family. I also enjoy quizzes and challenging myself on a variety of topics. And when I need to relax, there's nothing like traveling to new places and experiencing different cultures.
 
-Thanks for taking the time to read a little bit about me. I believe my combination of technical expertise, strong work ethic, and passion for learning make me a valuable asset to any team.`,
+Thank you for taking the time to read about me. I believe that my combination of technical expertise, strong work ethic, and passion for learning make me a valuable asset to any team.`,
     },
     interests: {
       football:
-        `Football has been a major part of my life since I was a child. My father instilled in me a love for the sport that has only grown stronger over the years. As a child, I spent countless hours playing with friends in the park or on the streets of my hometown.
+        `Soccer has been a big part of my life since I was a kid. My father instilled in me a love for the sport that has only grown stronger over the years. As a kid, I spent countless hours playing with friends in the park or on the streets of my hometown.
 
-My passion for football led me to pursue it professionally. I was fortunate enough to be invited to the Olympic Reserve football academy, where I received top-notch training from experienced coaches. The academy was a challenging but rewarding experience, and I learned a great deal about the sport and what it takes to succeed at the highest level.
+My passion for soccer led me to pursue it professionally. I was fortunate enough to be invited to the Olympic Reserve Football Academy, where I received top-notch training from experienced coaches. The Academy was a challenging but rewarding experience and I learned a lot about the sport and what it takes to succeed at the highest level.
 
-Playing football has taught me many valuable life lessons. It has taught me the importance of teamwork, perseverance, and dedication. It has also taught me how to handle success and failure with grace and humility.
+Playing soccer has taught me many valuable life lessons. It has taught me the importance of teamwork, perseverance and dedication. It has also taught me how to handle success and failure with grace and humility.
 
-To this day, I still love playing football whenever I have the chance. It's a great way to stay active, meet new people, and have fun. Even though I don't play professionally anymore, the lessons I learned from football continue to influence my life in a positive way.`,
+To this day, I still enjoy playing soccer whenever I get the chance. It's a great way to stay active, meet new people, and have fun. Even though I don't play professionally anymore, the lessons I've learned from soccer continue to have a positive impact on my life.`,
     },
     education: {
-      university: `Currently, I am pursuing higher education in the field of web development, and I couldn't be more excited about it. This program covers a wide range of disciplines that are essential to becoming a successful web developer.
+      university: `I am currently pursuing higher education in the field of web development, and I couldn't be more excited about it. This program covers a wide range of disciplines that are essential to becoming a successful web developer.
 
 One of the main areas of study is programming in JavaScript and Python. These programming languages are widely used in web development and are essential for creating interactive, user-friendly websites and applications. Through my coursework, I am gaining a deep understanding of the syntax, features, and best practices of both languages.
 
-Another key aspect of my education is learning about application architecture. This involves understanding how to design and structure complex web applications in a way that is scalable, maintainable, and efficient. This is essential knowledge for anyone looking to build robust, high-quality web applications.
+Another key aspect of my training is learning about application architecture. This includes understanding how to design and structure complex web applications in a way that is scalable, maintainable, and efficient. This is essential knowledge for anyone who wants to build robust, high-quality web applications.
 
-In addition to technical skills, my program also focuses on developing soft skills such as team management and goal achievement. These skills are critical for success in any industry, but especially in web development where collaboration and communication are key to delivering great results.
+In addition to technical skills, my program also focuses on developing soft skills such as team management and goal attainment. These skills are critical for success in any industry, but especially in web development, where collaboration and communication are key to achieving great results.
 
-Overall, I am deeply passionate about web development and am thrilled to be receiving a higher education in this field. The knowledge and skills I am gaining through my coursework will undoubtedly help me achieve my professional goals and make a meaningful impact in the world of web development.`,
+Overall, I am very passionate about web development and am thrilled to be pursuing higher education in this field. The knowledge and skills I am gaining through my coursework will undoubtedly help me achieve my professional goals and make a meaningful impact in the world of web development.`,
     },
   },
   professionalInfo: {
     experience: {
-      trello: `As a junior frontend developer at the startup, I was part of a team of 10 people developing an application similar to Trello. We used React, Redux, CSS Modules, Jest, and Enzyme to build the project. To improve our development process and the application itself, we also used Figma, a design tool that allowed our designers and developers to collaborate on designs in real-time.
+      trello: `As a junior frontend developer at the startup, I was part of a team of 10 people building an application similar to Trello. We used React, Redux, CSS modules, Jest, and Enzyme to build the project. To improve our development process and the application itself, we also used Figma, a design tool that allowed our designers and developers to collaborate on designs in real time.
 
-My main responsibility was to work on the frontend of the application, developing new features and fixing bugs. To ensure high-quality code, we used test-driven development (TDD) and wrote unit tests for our components. We also used continuous integration and continuous deployment (CI/CD) to streamline our development and deployment process. Travis CI was used to automatically build and test our code whenever changes were pushed to the repository. If the build and tests passed, the code was automatically deployed to a staging environment for further testing before being deployed to production.
+My main responsibility was to work on the front end of the application, developing new features and fixing bugs. To ensure high quality code, we used test-driven development (TDD) and wrote unit tests for our components. We also used continuous integration and continuous deployment (CI/CD) to streamline our development and deployment process. We used Travis CI to automatically build and test our code whenever changes were pushed to the repository. If the build and tests passed, the code was automatically deployed to a staging environment for further testing before being deployed to production.
 
-We used Git as a version control system and GitHub for managing our repositories. We also used JIRA for tracking issues and project management. Communication and collaboration were key to our success, and we held regular video conferences and used Slack for messaging and updates.
+We used Git as our version control system and GitHub to manage our repositories. We also used JIRA for issue tracking and project management. Communication and collaboration were key to our success, and we held regular video conferences and used Slack for messaging and updates.
 
-My experience with Figma was positive. It allowed us to collaborate more effectively on design decisions and ensured that our development was in line with the overall design of the application.
+My experience with Figma has been positive. It allowed us to collaborate more effectively on design decisions and ensured that our development was in line with the overall design of the application.
 
-Working in a team of 10 people provided me with valuable experience and skills in remote team communication, project management, TDD, and CI/CD. Overall, I enjoyed my time at the startup and am grateful for the opportunity to work on such an exciting project.`,
-      pdfTemplated: `I worked as part of a team developing a corporate system for working with PDF documents. Our system included a template engine that allowed users to create their own templates and add them to a publicly available library. These templates were built from customizable blocks, making them highly adaptable to a wide range of use cases.
+Working in a team of 10 people provided me with valuable experience and skills in remote team communication, project management, TDD, and CI/CD. Overall, I enjoyed my time at Startup and am grateful for the opportunity to work on such an exciting project.`,
+      pdfTemplated: `I was part of a team that developed an enterprise system for working with PDF documents. Our system included a template engine that allowed users to create their own templates and add them to a publicly available library. These templates were built from customizable blocks, making them highly adaptable to a wide range of use cases.
 
-Our main technology stack was built around React and MobX, which helped us create an intuitive and user-friendly interface. To ensure the quality of our code, we made use of linters and wrote tests to catch any potential issues early on.
+Our main technology stack was built around React and MobX, which helped us create an intuitive and user-friendly interface. To ensure the quality of our code, we used linters and wrote tests to catch potential issues early.
 
-Working alongside my team, we utilized GitLab as our version control system, which enabled us to collaborate efficiently and track changes effectively. We also followed agile methodologies to stay organized and focused on delivering high-quality results.
+Together with my team, we used GitLab as our version control system, which allowed us to collaborate efficiently and track changes effectively. We also followed agile methodologies to stay organized and focused on delivering quality results.
 
-For styling, we utilized Styled Components to create reusable and highly composable UI components. This allowed us to easily manage our stylesheets and ensure consistency across the entire application. And, to ensure everyone was working on the right tasks at the right time, we employed a task tracking system that kept us on track and on schedule.
+For styling, we used Styled Components to create reusable and highly composable UI components. This allowed us to easily manage our stylesheets and ensure consistency across the application. And to make sure everyone was working on the right tasks at the right time, we used a task tracking system that kept us on track and on schedule.
 
 Overall, working as part of a team on this project was a rewarding experience that helped me develop valuable skills as a developer.`,
 
